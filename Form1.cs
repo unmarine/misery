c2bac9dbec09561c7a6defc6c3e25a0b6cc4cb0f
davydkrysiuk <davydkrysiuk@A240400>
2026-02-05 14:57:24 +0000

    no idea, something stinky

--- Form1.cs
+++ Form1.cs
@@ -2,6 +2,7 @@ using misery.components;
 using misery.Components;
 using misery.eng;
 using misery.Eng;
+using misery.utils;
 
 namespace misery
 {
@@ -17,56 +18,57 @@ namespace misery
             this.ClientSize = new Size(2000, 2000);
             this.Text = "Automaton Simulation";
 
-            Settings.SetDefaultColorStatePairs(); // Presumed existing method
-            Settings.SetColorForState(2, Color.MediumVioletRed); // Set color for state
-            Settings.SetColorForState(1, Color.Yellow); // Set color for state
+            //Settings.SetDefaultColorStatePairs(); // Presumed existing method
+            //Settings.SetColorForState(2, Color.MediumVioletRed); // Set color for state
+            //Settings.SetColorForState(1, Color.Yellow); // Set color for state
 
-            INeighborhood neighborhood = new Moore(); // Assuming Moore is defined elsewhere
+            //INeighborhood neighborhood = new Moore(); // Assuming Moore is defined elsewhere
 
-            State dead = new State(0);
-            State live = new State(1);
-            State dying = new State(2);
-            State none = new State(-1);
+            //State dead = new State(0);
+            //State live = new State(1);
+            //State dying = new State(2);
+            //State none = new State(-1);
 
-            Condition first = new Condition(dead, live, live, 2, 2);
-            Condition second = new Condition(live, none, dying, -1, -1, true);
-            Condition third = new Condition(dying, none, dead, -1, -1, true);
+            //Condition first = new Condition(dead, live, live, 2, 2);
+            //Condition second = new Condition(live, none, dying, -1, -1, true);
+            //Condition third = new Condition(dying, none, dead, -1, -1, true);
 
-            RuleSet BriansBrain = new RuleSet();
-            BriansBrain.AddCondition(first);
-            BriansBrain.AddCondition(second);
-            BriansBrain.AddCondition(third);
+            //RuleSet BriansBrain = new RuleSet();
+            //BriansBrain.AddCondition(first);
+            //BriansBrain.AddCondition(second);
+            //BriansBrain.AddCondition(third);
 
-            first = new Condition(live, live, dead, 0, 1);
-            second = new Condition(live, live, live, 2, 3);
-            third = new Condition(live, live, dead, 4, 8);
-            var fourth = new Condition(dead, live, live, 3, 3);
+            //first = new Condition(live, live, dead, 0, 1);
+            //second = new Condition(live, live, live, 2, 3);
+            //third = new Condition(live, live, dead, 4, 8);
+            //var fourth = new Condition(dead, live, live, 3, 3);
 
-            RuleSet GameOfLife = new RuleSet();
-            GameOfLife.AddCondition(first);
-            GameOfLife.AddCondition(second);
-            GameOfLife.AddCondition(third);
-            GameOfLife.AddCondition(fourth);
+            //RuleSet GameOfLife = new RuleSet();
+            //GameOfLife.AddCondition(first);
+            //GameOfLife.AddCondition(second);
+            //GameOfLife.AddCondition(third);
+            //GameOfLife.AddCondition(fourth);
 
-            automaton = new Automaton(neighborhood, 200, 200, GameOfLife);
+            //automaton = new Automaton(neighborhood, 200, 200, GameOfLife);
 
             //automaton.Randomize(0, 1); // Presumed method to randomize grid
             
-            visualGrid = new VisualGrid(automaton.TheGrid); // Presumed class visualization
-            visualGrid.Height = 2000;
-            visualGrid.Width = 2000;
+            //visualGrid = new VisualGrid(automaton.TheGrid); // Presumed class visualization
+            //visualGrid.Height = 2000;
+            //visualGrid.Width = 2000;
 
-            timer = new System.Windows.Forms.Timer();
-            timer.Interval = 1; // Set refresh rate (500 ms
-            timer.Tick += Timer_Tick; // Attach tick event
-            RunPauseButton b = new RunPauseButton(timer);
+            //timer = new System.Windows.Forms.Timer();
+            //timer.Interval = 1; // Set refresh rate (500 ms
+            //timer.Tick += Timer_Tick; // Attach tick event
+            //RunPauseButton b = new RunPauseButton(timer);
             
-            this.Controls.Add(b);
-            this.Controls.Add(visualGrid);
+            //this.Controls.Add(b);
+            //this.Controls.Add(visualGrid);
 
             this.DoubleBuffered = true;
 
-
+            WindowManager wm = new WindowManager(this, 10, 10);
+            while (true) wm.Debug();
         }
 
         private void Timer_Tick(object sender, EventArgs e)
--- utils/WindowManager.cs
+++ utils/WindowManager.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,16 +10,28 @@ namespace misery.utils
     public class WindowManager
     {
         private Form _form;
-        
-        public WindowManager(Form form)
+        private Space _space; // haha
+
+        public WindowManager(Form form, int divisionsVertical, int divisionsHorizontal)
         {
             _form = form;
+            _space = new Space(form.Height, form.Width, divisionsVertical, divisionsHorizontal);
+        }
+
+        public void Debug()
+        {
+            Graphics g = _form.CreateGraphics();
+            foreach (var tile in _space.Tiles)
+            {
+                using Brush brush = new SolidBrush(Color.Red);
+                g.FillRectangle(brush, tile.Left, tile.Top, tile.Width, tile.Height);
+            }
         }
     }
 
     class Space
     {
-        Tile[,] tiles;
+        public Tile[,] Tiles;
         int _divisionsVertical, _divisionsHorizontal;
         int _height, _width;
 
@@ -36,7 +49,7 @@ namespace misery.utils
             _divisionHeight = height / divisionsVertical;
             _divisionWidth = width / divisionsHorizontal;
 
-            tiles = new Tile[divisionsVertical, divisionsHorizontal];
+            Tiles = new Tile[divisionsVertical, divisionsHorizontal];
 
             for (int row = 0; row < _divisionsVertical; row++)
             {
@@ -46,25 +59,16 @@ namespace misery.utils
                     int left = column * _divisionWidth;
 
                     Tile tile = new Tile(top, left, _divisionHeight, _divisionsVertical);
+
+                    Tiles[row, column] = tile;
                 }
             }
 
         }
 
-        public int GetHightBetween(Tile a, Tile b)
-        {
-            return Math.Abs(a.Top - b.Top);
-        }
-
-        public int GetWidthBetween(Tile a, Tile b)
-        {
-            return Math.Abs(a.Left - b.Left);
-        }
-
-        public (int, int) GetHeightWidthBetween(Tile a, Tile b)
-        {
-            return (GetHightBetween(a, b), GetWidthBetween(a, b));
-        }
+        public int GetHightBetween(Tile a, Tile b) => Math.Abs(a.Top - b.Top);
+        public int GetWidthBetween(Tile a, Tile b) => Math.Abs(a.Left - b.Left);
+        public (int, int) GetHeightWidthBetween(Tile a, Tile b) => (GetHightBetween(a, b), GetWidthBetween(a, b));
     }
 
     class Tile
