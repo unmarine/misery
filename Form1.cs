addf004def974f953637d450a1cf1e578cf9dfea
davydkrysiuk <davydkrysiuk@A240400>
2026-02-05 14:32:19 +0000

    Maybe complete failure. Attempt for managing space of the form

--- Form1.cs
+++ Form1.cs
@@ -14,7 +14,8 @@ namespace misery
 
         public Form1()
         {
-            InitializeComponent2(); // Make sure to call this first in the constructor.
+            this.ClientSize = new Size(2000, 2000);
+            this.Text = "Automaton Simulation";
 
             Settings.SetDefaultColorStatePairs(); // Presumed existing method
             Settings.SetColorForState(2, Color.MediumVioletRed); // Set color for state
@@ -47,7 +48,7 @@ namespace misery
             GameOfLife.AddCondition(third);
             GameOfLife.AddCondition(fourth);
 
-            automaton = new Automaton(neighborhood, 90, 90, GameOfLife); // Initialize automaton
+            automaton = new Automaton(neighborhood, 200, 200, GameOfLife);
 
             //automaton.Randomize(0, 1); // Presumed method to randomize grid
             
@@ -61,24 +62,17 @@ namespace misery
             RunPauseButton b = new RunPauseButton(timer);
             
             this.Controls.Add(b);
-            this.Controls.Add(visualGrid); // Add visual grid to form
-            this.DoubleBuffered = true; // Reduce flickering
+            this.Controls.Add(visualGrid);
+
+            this.DoubleBuffered = true;
 
 
         }
 
         private void Timer_Tick(object sender, EventArgs e)
         {
-            automaton.Advance(); // Move automaton forward
-            visualGrid.ReplaceGrid(automaton.TheGrid); // Update the visual grid
-        }
-
-
-        private void InitializeComponent2()
-        {
-            // Add standard initialization code here, e.g.:
-            this.ClientSize = new Size(2000, 2000);
-            this.Text = "Automaton Simulation";
+            automaton.Advance();
+            visualGrid.ReplaceGrid(automaton.TheGrid);
         }
     }
 }
---
+++ utils/WindowManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace misery.utils
+{
+    public class WindowManager
+    {
+        private Form _form;
+        public WindowManager(Form form)
+        {
+            _form = form;
+        }
+    }
+
+    class Space
+    {
+        Tile[,] tile;
+
+        public Space()
+        {
+
+        }
+
+        public int GetHightBetween(Tile a, Tile b)
+        {
+            return Math.Abs(a.top - b.top);
+        }
+
+        public int GetWidthBetween(Tile a, Tile b)
+        {
+            return Math.Abs(a.left - b.left);
+        }
+
+        public (int, int) GetHeightWidthBetween(Tile a, Tile b)
+        {
+            return (GetHightBetween(a, b), GetWidthBetween(a, b));
+        }
+    }
+
+    class Tile
+    {
+        public 
+        int top, left;
+    }
+}
