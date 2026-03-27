9efdc743d83084b2175defc0d617dde4525c6526
davydkrysiuk <davydkrysiuk@A240574>
2026-03-27 10:42:50 +0000

    tertert

--- components/pathfinding/SetEndButton.cs
+++ components/pathfinding/SetEndButton.cs
@@ -1,7 +1,15 @@
 ﻿namespace misery.components.pathfinding;
 
-public class SetEndButton(InteractiveGrid interactiveGrid) : Button
+public class SetEndButton : Button
 {
+    private readonly InteractiveGrid interactiveGrid;
+
+    public SetEndButton(InteractiveGrid interactiveGrid)
+    {
+        this.interactiveGrid = interactiveGrid;
+        Text = "Set End";
+    }
+
     protected override void OnClick(EventArgs e)
     =>
         interactiveGrid.CurrentMode = InteractiveGridMode.SetEnd;
--- components/pathfinding/SetStartButton.cs
+++ components/pathfinding/SetStartButton.cs
@@ -1,7 +1,15 @@
 ﻿namespace misery.components.pathfinding;
 
-public class SetStartButton(InteractiveGrid interactiveGrid): Button
+public class SetStartButton : Button
 {
+    private readonly InteractiveGrid interactiveGrid;
+
+    public SetStartButton(InteractiveGrid interactiveGrid)
+    {
+        this.interactiveGrid = interactiveGrid;
+        Text = "Set Start";
+    }
+
     protected override void OnClick(EventArgs e)
     =>
         interactiveGrid.CurrentMode = InteractiveGridMode.SetStart;
